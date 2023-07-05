007fdee0db11402bd93ca47fbe3a9b33ed59904f
yuu/green <yuu-akiyama@greensys.co.jp>
2023-07-05 09:53:40 +0900

    初期コミット

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by 秋山　悠 on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
