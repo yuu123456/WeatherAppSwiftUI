3652f036226920712d784cd4481a2467fcb5bb38
yuu/green <yuu-akiyama@greensys.co.jp>
2023-07-06 21:16:24 +0900

    main merge コンフリクト解消のため、新規ブランチへ

---
+++ WeatherAppSwiftUI/Extension/ExButton.swift
@@ -0,0 +1,21 @@
+//
+//  ExButton.swift
+//  WeatherAppSwiftUI
+//
+//
+import SwiftUI
+
+extension Button {
+    ///メイン画面用のカスタムモディファイアメソッド
+    func mainViewButtonModefier(width: Double) -> some View {
+        self
+            // テキストを省略させないモディファイア
+            .fixedSize()
+            .font(.title)
+            .padding()
+            .frame(width: width)
+            .foregroundColor(.black)
+            .background(Color.green)
+            .cornerRadius(20)
+    }
+}
---
+++ WeatherAppSwiftUI/View/MainView.swift
@@ -0,0 +1,99 @@
+//
+//  MainView.swift
+//  WeatherAppSwiftUI
+//
+//
+import SwiftUI
+
+// View
+struct MainView: View {
+    @StateObject private var mainViewModel = MainViewModel()
+
+    /// ナビゲーションバーの設定を行うメソッド
+    func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .darkGray
+        appearance.shadowColor = .black
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        // ナビゲーションバーに反映
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    init() {
+        setupNavigationBar()
+    }
+
+    // 選択画面に遷移するボタン
+    var toSelectPrefectureViewButton: some View {
+        NavigationStack {
+            Button {
+                mainViewModel.tappedSelectPrefectureButton()
+            } label: {
+                Label("都道府県を選択", systemImage: "location")
+            }
+            .mainViewButtonModefier(width: mainViewModel.buttonWidth)
+        }
+        .navigationDestination(isPresented: $mainViewModel.isSelectPrefectureButtonTapped) {
+            SelectView()
+        }
+    }
+    // 詳細画面に遷移するボタン
+    var toDetailViewButton: some View {
+        NavigationStack {
+            Button {
+                mainViewModel.tappedGetLocationButton()
+            } label: {
+                Label("現在地を取得", systemImage: "list.bullet")
+            }
+            .mainViewButtonModefier(width: mainViewModel.buttonWidth)
+        }
+        //モーダル遷移
+        .sheet(isPresented: $mainViewModel.isGetLocationButtonTapped) {
+            DetailView()
+        }
+    }
+
+    //ナビゲーションバーに表示する通知ボタン
+    var notificationButton: some View {
+        Button {
+            //ダイアログ出す処理を記述
+            
+        } label: {
+            Image(systemName: mainViewModel.noticationImageName())
+                .tint(.yellow)
+        }
+    }
+
+    //背景色
+    var backgroundView: some View {
+        // 画面いっぱいの背景色（グラデーション）
+        LinearGradient(gradient: Gradient(colors: [.cyan, .white]), startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+    }
+
+    var body: some View {
+        ZStack {
+            backgroundView
+            VStack(spacing: 50) {
+                toSelectPrefectureViewButton
+                toDetailViewButton
+            }
+        }
+        .navigationTitle(Text("Home"))
+        .navigationBarTitleDisplayMode(.inline)
+        // 戻るボタンを非表示
+        .navigationBarBackButtonHidden()
+        .toolbar() {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                notificationButton
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
