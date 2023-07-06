2f1145ce360bbaf21be8a8164d53362b508ce0ac
yuu/green <yuu-akiyama@greensys.co.jp>
2023-07-06 10:02:34 +0900

    スプラッシュ画面アニメーション実装

--- WeatherAppSwiftUI/ViewModel/SplashView.swift
+++ WeatherAppSwiftUI/ViewModel/SplashView.swift
@@ -8,50 +8,77 @@
 import SwiftUI
 
 struct SplashView: View {
-    // 画面遷移可否を示す状態変数
+    /// 画面遷移可否を示す状態変数
     @State private var isActive = false
-    
+    /// アニメーション可否を示す変数
+    @State var isAnimation = false
     /// スクリーンのサイズ取得し、３分の１とする（見栄え）
     private let imageWidth = UIScreen.main.bounds.width / 3
+    
+    private var rainImage: some View {
+        Image(systemName: "cloud.rain")
+            .splashImageModefier(width: imageWidth)
+            .offset(x: isAnimation ? -20 : 20)
+            .animation(.spring(dampingFraction: 0).repeatForever(), value: isAnimation)
+            .foregroundColor(.blue)
+    }
+    private var sunImage: some View {
+        Image(systemName: "sun.max.fill")
+            .splashImageModefier(width: imageWidth)
+            .rotationEffect(Angle(degrees: isAnimation ? 0 : 360))
+            .animation(.linear(duration: 3).repeatForever(autoreverses: false), value: isAnimation)
+            .foregroundColor(.red)
+    }
+    private var cloudImage: some View {
+        Image(systemName: "cloud.fill")
+            .splashImageModefier(width: imageWidth)
+            .scaleEffect(isAnimation ? 10 : 1)
+            .animation(.easeIn(duration: 3), value: isAnimation)
+            .foregroundColor(.gray)
+            // Viewの表示順序を制御するモディファイア（基本はコードの下方が最前面にくるため）
+            .zIndex(-1)
+    }
+    private var boltImage: some View {
+        Image(systemName: "cloud.bolt.rain.fill")
+            .splashImageModefier(width: imageWidth)
+            .offset(y: isAnimation ? -10 : 10)
+            .animation(.easeInOut(duration: 1.5).repeatForever(), value: isAnimation)
+            .foregroundColor(.yellow)
+    }
+    private var snowImage: some View {
+        Image(systemName: "cloud.snow")
+            .splashImageModefier(width: imageWidth)
+            .opacity(isAnimation ? 0 : 1)
+            .animation(.easeInOut(duration: 0.5).repeatForever(), value: isAnimation)
+            .foregroundColor(.cyan)
+    }
     
     var body: some View {
         // 画面遷移の設定
         NavigationStack {
             VStack {
                 HStack {
-                    Image(systemName: "cloud.rain")
-                        .splashImageModefier(width: imageWidth)
-                        .foregroundColor(.blue)
-                    Image(systemName: "sun.max.fill")
-                        .splashImageModefier(width: imageWidth)
-                        .foregroundColor(.red)
+                    rainImage
+                    sunImage
                 }
-                
-                Image(systemName: "cloud.fill")
-                    .splashImageModefier(width: imageWidth)
-                    .foregroundColor(.gray)
-//                    .padding()
+                cloudImage
                 HStack {
-                    Image(systemName: "cloud.bolt.rain.fill")
-                        .splashImageModefier(width: imageWidth)
-                        .foregroundColor(.yellow)
-                    Image(systemName: "cloud.snow")
-                        .splashImageModefier(width: imageWidth)
-                        .foregroundColor(.cyan)
+                    boltImage
+                    snowImage
                 }
             }
-            .padding()
+            .onAppear() {
+                // 表示と共にアニメーション起動
+                isAnimation.toggle()
+                // 現在時刻から３秒後に行う処理（時間経過後に自動遷移。タイマーでは画面タップしないと遷移しないため）
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    isActive.toggle()
+                }
+            }
             // isPresentedの引数trueになればナビゲーション遷移
             .navigationDestination(isPresented: $isActive) {
                 // 遷移先画面
                 MainView()
-            }
-        }
-        // 画面が表示された時の処理を記述
-        .onAppear() {
-            // 現在時刻から３秒後に行う処理（時間経過後に自動遷移。タイマーでは画面タップしないと遷移しないため）
-            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
-                isActive = true
             }
         }
     }
