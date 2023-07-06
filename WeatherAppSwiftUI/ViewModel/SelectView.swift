210e201410a68f95eac0efe8732f6c4b2b259a5e
yuu/green <yuu-akiyama@greensys.co.jp>
2023-07-06 21:39:55 +0900

    View分割

--- WeatherAppSwiftUI/ViewModel/SelectView.swift
+++ WeatherAppSwiftUI/ViewModel/SelectView.swift
@@ -8,28 +8,29 @@
 import SwiftUI
 
 struct SelectView: View {
-    @State var isAvtive = false
+    @StateObject var selectViewModel = SelectViewModel()
+    
     /// 詳細画面に遷移するボタン
-    var toDetailViewButton: some View {
+    var prefecturesList: some View {
         NavigationStack {
             List {
                 // 配列の数だけ繰り返し、個別のidとして自身の値を利用する
                 ForEach(PrefectureData.prefectures, id:\.self) {prefecture in
                     Button("\(prefecture)") {
-                        isAvtive = true
+                        selectViewModel.tappedCell()
                     }
                     //モーダル遷移
-                    .sheet(isPresented: $isAvtive) {
+                    .sheet(isPresented: $selectViewModel.isCellTapped) {
                         DetailView()
                     }
                 }
             }
         }
+        .navigationTitle(Text("都道府県の選択"))
     }
     
     var body: some View {
-        toDetailViewButton
-        .navigationTitle(Text("都道府県の選択"))
+        prefecturesList
     }
 }
 
